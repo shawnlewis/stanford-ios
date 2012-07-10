1cf72f267f0edfcc6d18f60648a36cb92771b642
Shawn Lewis <shlewis@gmail.com>
2012-07-09 23:08:58 -0700

    specify main

--- a2-clj/project.clj
+++ a2-clj/project.clj
@@ -1,4 +1,5 @@
 (defproject a2-clj "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [seesaw "1.2.2"]])
+                 [seesaw "1.2.2"]]
+  :main a2-clj.core)
