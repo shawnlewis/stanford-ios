a741079a91b6684cca9307764ad7ee5678526fb0
Shawn Lewis <shlewis@gmail.com>
2012-07-24 17:18:37 -0700

    Use runProgram instead of a stack

--- l2/Calculator/Calculator/CalculatorBrain.h
+++ l2/Calculator/Calculator/CalculatorBrain.h
@@ -13,4 +13,9 @@
 - (void)pushOperand:(double)operand;
 - (double)performOperation:(NSString* )operation;
 
+@property (readonly) id program;
+
++ (double)runProgram:(id)program;
++ (NSString *)descriptionOfProgram:(id)program;
+
 @end
